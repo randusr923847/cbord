63d9818ace7e18afdb7ab4eb2569e61bd603e80e
Taran Kamireddy <59002695+TaranKamireddy@users.noreply.github.com>
2025-08-14 20:08:25 -0400

    rolled back to old data.ts

--- src/db.ts
+++ src/db.ts
@@ -1,37 +1,28 @@
-// import mongoose from 'mongoose';
-// import config from './config';
-
-// let db_url = 'mongodb://';
-
-// if (config.db.username && config.db.password) {
-//   db_url += `${config.db.username}:${config.db.password}@`;
-// }
-
-// db_url += `${config.db.host}:${config.db.port}/${config.db.database}`;
-
-// // MongoDB Connection
-// mongoose
-//   .connect(db_url)
-//   .then(() => console.log('MongoDB connected'))
-//   .catch((err) => console.error('MongoDB connection error:', err));
-
-// const db = mongoose.connection;
-// db.on('error', function (e) {
-//   console.log('Error with MongoDB: ' + e);
-//   process.exit(1);
-// });
-
-// db.once('open', function () {
-//   console.log(`Successfully connected to MongoDB on port ${config.db.port}`);
-// });
-
-// export default mongoose;
-
-//taran trying mongo
 import mongoose from 'mongoose';
+import config from './config';
+
+let db_url = 'mongodb://';
+
+if (config.db.username && config.db.password) {
+  db_url += `${config.db.username}:${config.db.password}@`;
+}
+
+db_url += `${config.db.host}:${config.db.port}/${config.db.database}`;
+
+// MongoDB Connection
 mongoose
-  .connect('mongodb://localhost:27017/bord')
+  .connect(db_url)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
-// console.log(mongoose.connection);
+
+const db = mongoose.connection;
+db.on('error', function (e) {
+  console.log('Error with MongoDB: ' + e);
+  process.exit(1);
+});
+
+db.once('open', function () {
+  console.log(`Successfully connected to MongoDB on port ${config.db.port}`);
+});
+
 export default mongoose;

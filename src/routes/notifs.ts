f60b645b8579aa2df7907aafff1832d6921f1ce6
Omkar <omkar@purdue.edu>
2025-09-01 16:53:00 -0400

    add email list endpoint

--- src/routes/notifs.ts
+++ src/routes/notifs.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import Email from '../models/email';
+import '../types/session';
 
 const router = express.Router();
 
@@ -23,6 +24,17 @@ router.post('/unsubscribe', async (req, res) => {
     res.json({ success: true });
   } else {
     res.json({ success: false });
+  }
+});
+
+router.get('/list', async (req, res) => {
+  if (req.session.auth) {
+    const data = await Email.find({}).lean();
+    const list = data.map((o) => o.email);
+    const out = list.join(',');
+    res.json({ data: out });
+  } else {
+    res.render('404');
   }
 });
 
